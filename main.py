b74d05a6f6e63231a33280aac565221e6535e314
Javier Almarza <javieralmarzabucarey@gmail.com>
2021-04-07 10:53:34 -0400

    primeras configuraciones

---
+++ main.py
@@ -0,0 +1,42 @@
+# Desarrollo de un concatenador de alfabetos.
+# Autor: Javier Almarza.
+
+class MasterManager():
+    # Propiedades.
+    # Constructor.
+
+    # Metodos.
+    def compiladorLaTeX(self):
+        pass
+    
+    def manejadorArchivos(self, operador):
+        # Lecturas de archivos.
+        if(operador == 1):
+            try:        
+                with open("input", "rt") as Archivo:
+                    print(f"[CONTENT] {Archivo.read()}")
+            except FileNotFoundError:
+                print(f"[ERROR] El archivo input no fue creado.")    
+        # Escritura de archivos.
+        elif(operador == 2):
+            pass
+
+        else:
+            print(f"[ERROR] Ocurrio un error al administrar los archivos.")
+
+    def menu(self):
+        # 1. Iteracion de Lenguajes.
+        # 2. Determinación de la pertenencia de un Lenguaje.
+        # 3. Concatenación de dos cadenas.
+        pass
+
+    def administradorCadenas(self):
+        
+        pass
+
+    # Getters & Setters.
+    pass
+
+# Instancias de la clase.
+MM = MasterManager()
+MM.manejadorArchivos(1)
---
+++ readme.md
@@ -0,0 +1,4 @@
+# Concatenador de Alfabetos
+
+## Ejemplos
+Dentro del archivo **examples**, podremos identificar como deben ser estructurados los datos en el archivo **input** de datos para cada uno de los problemas a resolver.
