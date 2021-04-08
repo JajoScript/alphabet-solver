6cd03fa469f4f4f0824517cff3fdd19df16567fa
Javier Almarza <javieralmarzabucarey@gmail.com>
2021-04-08 00:39:33 -0400

    primer algoritmo terminado

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # Dependencias.
 import os
 import time
+import numpy as np
 
 # Definición de clase.
 class MasterManager():
@@ -11,24 +12,27 @@ class MasterManager():
 
     # Constructor.
     def __init__(self, modo):
-        self.__modoDepurador = modo
+        # Configuración
+        self.modoDepurador = modo
+        self.__lineasArchivo = 5
+
+        # Colores 
         self.__normal = '\033[92m'
         self.__error = '\033[91m'
         self.__dev = '\033[93m'
         self.__end = '\033[0m'
-        self.__lineasArchivo = 5
 
     # Metodos.
-    def __registro(self, id,contenido):
+    def __registro(self, id, contenido, segundos=2):
         tiempo = time.localtime(time.time())
 
         if id == "DEV" and self.modoDepurador == True:
             print(self.__dev + f"[{tiempo.tm_hour}:{tiempo.tm_min}:{tiempo.tm_sec}][{id}] {contenido}" + self.__end)
-            self.__tiempoEspera(1)
+            self.__tiempoEspera(segundos)
 
         elif id == "ERROR":
             print(self.__error + f"[{tiempo.tm_hour}:{tiempo.tm_min}:{tiempo.tm_sec}][{id}] {contenido}" + self.__end)
-            self.__tiempoEspera(2)
+            self.__tiempoEspera(segundos)
 
         else:
             print(self.__normal + f"[{tiempo.tm_hour}:{tiempo.tm_min}:{tiempo.tm_sec}][{id}] {contenido}" + self.__end)
@@ -46,7 +50,8 @@ class MasterManager():
             try:        
                 with open(nombreArchivo, "rt") as Archivo:
                     if configuracion == "iteracion":
-                        
+                        datos = []
+
                         linea = 1
                         while(linea <= self.__lineasArchivo): 
                             contenido = Archivo.readline()
@@ -54,8 +59,9 @@ class MasterManager():
                                 # Determinando el lenguaje.
                                 if contenido[0] == "L":
                                     self.__registro("DEV", "Pille el lenguaje")
-                                    array = contenido[3:-2].split(",")
-                                    self.__registro("DEV", array)
+                                    lenguaje = contenido[3:-2].split(",")
+
+                                    self.__registro("DEV", lenguaje)
 
                                 # Determinando la iteración.
                                 else:
@@ -66,12 +72,21 @@ class MasterManager():
                                 
 
                             linea += 1
+                        
+                        # Almacenando los datos.
+                        datos.append(lenguaje)
+                        datos.append(iteracion)
+
+
+                        self.__registro("DEV", datos)
+                        return datos
+
 
                     elif configuracion == "determinacion":
-                        print(f"[DEV] {Archivo.read()}")
+                        self.__registro("DEV", Archivo.read())
 
                     elif configuracion == "concatenacion":
-                        print(f"[DEV] {Archivo.read()}")
+                        self.__registro("DEV", Archivo.read())
 
             except FileNotFoundError:
                 self.__controladorErrores("No se encontro el archivo especificado.")
@@ -99,7 +114,31 @@ class MasterManager():
 
         # Apertura de archivo.
         nombreArchivo = input("[>] Ingresa el nombre del archivo (input): ")
-        self.__manejadorArchivos("lectura", nombreArchivo, "iteracion", "")
+        datos = self.__manejadorArchivos("lectura", nombreArchivo, "iteracion", "")
+
+        # información.
+        alfabeto = datos[0]
+        tamanoAlfabeto = datos[1]
+
+        iteracion = 1
+        while iteracion <= tamanoAlfabeto:
+            # estado inicial.
+            if iteracion == 1:
+                lenguaje = alfabeto
+                lenguajeNuevo = lenguaje
+            else:
+                lenguaje = list(lenguajeNuevo)
+
+                cadena = ""
+                lenguajeNuevo = []
+                for a in alfabeto: # {a,b}
+                    for b in lenguaje: # {a,b} 
+                        cadena = a + b # aa, ab, ba, bb
+                        lenguajeNuevo.append(cadena)
+
+            self.__registro("DEV", f"{lenguajeNuevo} : iteracion {iteracion} : elementos {len(lenguajeNuevo)}", 5)
+            iteracion += 1
+            
 
 
     def __determinacionLenguaje(self):
